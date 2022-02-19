59b407da9a48765ef6449407b849e9a1b563b3a0
AgatyaP <itsagatya07@gmail.com>
2022-02-19 23:19:29 +0530

    <added carousel>

---
+++ home/static/home.js
@@ -0,0 +1,104 @@
+
+    $(".owl-one").owlCarousel({
+        loop : true,
+        nav : true,
+        items: 2,
+        margin : 5,
+        padding : 5,
+        stagePadding : 5,
+        responsive : {
+            0 : {
+                items: 1,
+                dots : false
+            },
+            485 : {
+                items: 2,
+                dots : false
+            },
+            728 : {
+                items: 3,
+                dots : false
+            },
+            940 : {
+                items: 4,
+                dots : true
+            },
+            1200 : {
+                items: 2,
+                dots : true
+            }
+        }
+         
+    }
+        
+    );
+
+    
+    $(".owl-two").owlCarousel({
+        loop : true,
+        nav : true,
+        items: 2,
+        margin : 5,
+        padding : 5,
+        stagePadding : 5,
+        responsive : {
+            0 : {
+                items: 1,
+                dots : false
+            },
+            485 : {
+                items: 1,
+                dots : false
+            },
+            728 : {
+                items: 2,
+                dots : false
+            },
+            940 : {
+                items: 2,
+                dots : true
+            },
+            1200 : {
+                items: 3,
+                dots : true
+            }
+        }
+         
+    }
+        
+    );
+
+    $(".owl-three").owlCarousel({
+        loop : true,
+        nav : true,
+        items: 2,
+        margin : 5,
+        padding : 5,
+        stagePadding : 5,
+        responsive : {
+            0 : {
+                items: 1,
+                dots : false
+            },
+            485 : {
+                items: 1,
+                dots : false
+            },
+            728 : {
+                items: 2,
+                dots : false
+            },
+            940 : {
+                items: 2,
+                dots : false
+            },
+            1200 : {
+                items: 2,
+                dots : false
+            }
+        }
+         
+    }
+        
+    );
+
